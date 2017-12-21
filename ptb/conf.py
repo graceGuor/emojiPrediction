b5e18758ccb566e2a5427bd7f2c8cbf44fe0a04d
grace <grace_guor@163.com>
2017-12-21 14:25:12 +0800

    2w_200d savepath

--- ptb/conf.py
+++ ptb/conf.py
@@ -2,7 +2,7 @@
 src_path = "/home/pubsrv/data/guorui/data"
 data_path = src_path + "/Fold/0"
 save_path = src_path + "/Res/Fold1/0"
-emb_path = src_path + "/glove.twitter.27B/glove.twitter.27B.200d.txt"
+emb_path = src_path + "/glove.twitter.27B/embeddding_2w_200d.txt"
 num_GPU = 1
 max_max_epoch = 13#55#需要修改
 hidden_size = 200#400#20#需要修改
