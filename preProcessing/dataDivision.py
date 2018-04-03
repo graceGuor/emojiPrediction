b7f070d4d1abaf5b60bdd54642475a59f711311f
graceGuor <34294805+graceGuor@users.noreply.github.com>
2018-04-03 11:43:50 +0800

    rand_emojiCoOccur_scale_part

--- preProcessing/dataDivision.py
+++ preProcessing/dataDivision.py
@@ -6,6 +6,13 @@ import sys
 def dataDivision811(srcPath,resPath, fold):
     f_sour = open(srcPath,'r',encoding='utf8')
     lines = f_sour.readlines()
+
+    # 取总数据的1/10
+    # l = len(lines)
+    # part = int(0.1 * l)
+    # print(part)
+    # lines = lines[0: part]
+
     print('all data size:' + str(len(lines)))
     sizeOfFold = int(len(lines)/fold)
     print('size of each fold:' + str(sizeOfFold))
@@ -23,7 +30,7 @@ def dataDivision811(srcPath,resPath, fold):
     # f_res.writelines(lines[sizeOfFold * 2:])
 
     if not os.path.exists(resPath + '/' + str(f)):
-        os.mkdir(resPath + '/' + str(f))
+        os.mkdir(os.path.join(resPath, str(f)))
         print('Successfully created directory', resPath + '/' + str(f))
     f_res = open(resPath + '/' + str(f) + '/test.txt','w',encoding='utf8')
     f_res.writelines(lines[:sizeOfFold])
@@ -36,7 +43,10 @@ def dataDivision811_main():
     srcPath = 'E:\Data\EmojiPrediction\emoji_sample_head_unk_delProportion.txt'
     resPath = 'E:\Data\EmojiPrediction\Fold'
     srcPath = conf.alldata_path
-    resPath = conf.src_path + '/Fold'
+    resPath = os.path.join(conf.src_path, 'Fold_part')
+    if not os.path.exists(resPath):
+        os.mkdir(resPath)
+        print('Successfully created directory', resPath)
     dataDivision811(srcPath, resPath, conf.fold)
 
 if __name__ == "__main__":
